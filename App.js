79f72f420a913a7fabb00c86807a80b11132412d
Scott Sigman <ssigman@deuey.wdu>
2023-01-26 12:23:58 -0600

    Inital Proj Config

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import HomeScreen from './screens/HomeScreen';
+import Location from './screens/Location';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const App =() => {
+    return (
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                if (route.name ==='Home') {
+                  iconName = focused 
+                    ? 'ios-home'
+                    : 'ios-home';
+                } else if (route.name==='Location') {
+                  iconName = focused ? 'ios-list' : 'ios-list';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />
+              },
+              tabBarActiveTintColor: '#03DAC6',
+              tabBarInactiveTintColor: '#018786',
+              tabBarActiveBackgroundColor: '#6200EE',
+              tabBarInactiveBackgroundColor: '#3700B3'
+            })}
+          >
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={ {
+                title: 'MyWeather', 
+                headerStyle: {
+                  backgroundColor: '#6200EE',
+                },
+                headerTitleStyle: {
+                  color: '#fff',
+                },
+              }}
+            />
+            <Tab.Screen 
+              name ='Location'
+              component={Location}
+              options={ {
+                title: 'Locations',
+                headerStyle: {
+                  backgroundColor: '#6200EE',
+                },
+                headerTitleStyle: {
+                  color: '#fff',
+                },
+              }} 
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +72,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
